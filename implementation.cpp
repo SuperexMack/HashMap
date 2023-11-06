5cbb5bc26dc568730308131455a787f51ecc53dd
Mohit Sati <146699961+SuperexMack@users.noreply.github.com>
2023-11-06 12:27:49 +0530

    Update implementation.cpp

--- implementation.cpp
+++ implementation.cpp
@@ -1,90 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
-// creation of a map
-unordered_map<string,int>mack1;
+    // creation of a map
+    unordered_map<string, int> mack1;
 
+    // insertion
 
-// insertion
+    // first way
 
-// first way
+    pair<string, int> mack2 = make_pair("mack", 3);
 
-pair<string,int> mack2 = make_pair("mack" ,3);
+    mack1.insert(mack2);
 
-mack1.insert(mack2);
+    // second method
 
+    pair<string, int> pair2("mohit", 2);
+    mack1.insert(pair2);
 
-// second method
+    // third method
 
-pair<string , int>pair2("mohit" , 2);
-mack1.insert(pair2);
+    mack1["sati"] = 1;
 
+    // searching
 
-// third method
+    cout << mack1["mack"] << endl;     // ans will be 3
+    cout << mack1.at("mohit") << endl; // ans will be 2
+    // cout<<mack1.at("unKnownkey")<<endl; it will give an error
+    cout << mack1["unknown"] << endl; // it will give output as 0 it will not give any error
+    cout << mack1.size() << endl;     // it will cout entry of [ ] wala map
+    cout << mack1.count("rahul") << endl;
+    cout << mack1.count("mack") << endl;
 
-mack1["sati"] = 1;
+    // note count will return 1 if that element is present in the map otherwise it will return 0
 
-// searching
+    mack1.erase("sati"); // it will remove that element from the map
 
-cout<<mack1["mack"]<<endl; // ans will be 3
-cout<<mack1.at("mohit")<<endl; // ans will be 2
-// cout<<mack1.at("unKnownkey")<<endl; it will give an error
-cout<<mack1["unknown"]<<endl; // it will give output as 0 it will not give any error
-cout<<mack1.size()<<endl; // it will cout entry of [ ] wala map
-cout<<mack1.count("rahul")<<endl;
-cout<<mack1.count("mack")<<endl;
+    cout << mack1.size() << endl;
 
-// note count will return 1 if that element is present in the map otherwise it will return 0
+    // accessing the whole map
 
-mack1.erase("sati"); // it will remove that element from the map
+    cout << "Accessing the whole map" << endl;
 
-cout<<mack1.size()<<endl; 
+    for (auto i : mack1)
+    {
 
-// accessing the whole map
+        cout << i.first << " " << i.second << endl;
+    }
 
-cout<<"Accessing the whole map"<<endl;
+    cout << endl;
 
-for(auto i : mack1){
+    cout << endl;
 
-cout<<i.first<<" "<<i.second<<endl;
+    // iterator for accessing the whole unorderedMap
 
-}
+    cout << "Accessing the whole unorderedMap using iterator" << endl;
 
+    unordered_map<string, int>::iterator it = mack1.begin();
 
-cout<<endl;
+    while (it != mack1.end())
+    {
 
-cout<<endl;
+        cout << it->first << " " << it->second << endl;
+        it++;
+    }
 
-// iterator for accessing the whole unorderedMap 
+    // whenever we use unordered map so during printing of element the element do not asynchronus wise
 
-cout<<"Accessing the whole unorderedMap using iterator"<<endl;
+    // whenever we use map then the time complexity is O(logN) and when we use unordered list then time complexity is O(1).
 
-unordered_map<string , int> :: iterator it = mack1.begin();
-
-while(it != mack1.end()){
-
-cout<<it->first<<" "<<it->second<<endl;
-it++;
-
-
-}
-
-
-// whenever we use unordered map so during printing of element the element do not asynchronus wise
-
-// whenever we use map then the time complexity is O(logN) and when we use unordered list then time complexity is O(1).
-
-
-
-
-
-
-
-
-
-return 0;
-
-
+    return 0;
 }
